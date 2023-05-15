68ab010e013cd0c9e93020c72eb24e158e6a4558
Erika Balog <enbalog@gmail.com>
2023-05-15 15:22:08 +0200

    Implemented more routes and views

--- tastyer/routes/tastyer.routes.js
+++ tastyer/routes/tastyer.routes.js
@@ -2,9 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/home", (req, res, next) => {
   res.render("home"); //reminder that this is a file reference, not a route reference
 });
+
+/* GET index page */
+router.get("/", (req, res, next) => {
+  res.render("index"); 
+});
+
+/* GET recipeId page */
+router.get("/recipeId", (req, res, next) => {
+  res.render("recipe"); 
+});
+
+/* GET aboutUs page */
+router.get("/aboutUs", (req, res, next) => {
+  res.render("aboutUs"); 
+});
+
+/* GET allRecipes page */
+router.get("/allrecipes", (req, res, next) => {
+  res.render("allrecipes"); 
+});
+
+
 
 
 
