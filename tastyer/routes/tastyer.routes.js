1a98ba40677bd51724bebeb5a07ea38190cdf45e
Erika Balog <enbalog@gmail.com>
2023-05-16 14:27:32 +0200

    Implemented axios in home route and recipeId route

--- tastyer/routes/tastyer.routes.js
+++ tastyer/routes/tastyer.routes.js
@@ -1,60 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 const options = {
-  method: 'GET',
-  url: 'https://tasty.p.rapidapi.com/recipes/list',
+  method: "GET",
+  url: "https://tasty.p.rapidapi.com/recipes/list",
   headers: {
-    'X-RapidAPI-Key': '6516624676msh84b669094735db6p107d62jsn77af1d574a92',
-    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+    "X-RapidAPI-Key": "6516624676msh84b669094735db6p107d62jsn77af1d574a92",
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
   },
   params: {
-    from: '0',
-    size: '1',
-  }
+    from: "0",
+    size: "5",
+  },
 };
-const Recipe = require('../models/Recipe.model')
+const Recipe = require("../models/Recipe.model");
 
 /* GET index page */
-router.get("/",  (req, res, next) => {
-  res.render("index"); 
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
-
 
 /* GET home page */
 router.get("/home", (req, res, next) => {
-  axios.request(options)
-.then(response => {
-  console.log('recipes', response.data.results);
-  res.render("home", {recipes: response.data.results}); 
-})
-.catch (err => {
-  (console.log(err));
+  options.url = "https://tasty.p.rapidapi.com/recipes/list";
+  options.params = {
+    from: "0",
+    size: "5",
+  };
+  console.log('option.params!!!!!!!!!!!!', options.params)
+
+  axios
+    .request(options)
+    .then((response) => {
+      // console.log("recipes", response.data.results);
+      res.render("home", { recipes: response.data.results });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-  });
 
 /* GET recipeId page */
-router.get("/recipeId", (req, res, next) => {
-  res.render("recipe"); 
+router.get("/:recipeId", (req, res, next) => {
+  options.url = `https://tasty.p.rapidapi.com/recipes/get-more-info`;
+  options.params = { id: req.params.recipeId }
+  console.log('option.params!!!!!!!!!!!!', options.params)
+  axios
+    .request(options)
+    .then((response) => {
+      console.log("recipe---------------", response.data);
+      res.render("recipe", { recipe: response.data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 /* POST recipeId page */
 router.post("/recipeId", (req, res, next) => {
-  res.redirect("recipe"); 
+  res.redirect("recipe");
 });
 
 /* GET aboutUs page */
 router.get("/aboutUs", (req, res, next) => {
-  res.render("aboutUs"); 
+  res.render("aboutUs");
 });
 
 /* GET allRecipes page */
 router.get("/allrecipes", (req, res, next) => {
-  res.render("allrecipes"); 
+  res.render("allrecipes");
 });
-
-
-
-
 
 module.exports = router;
